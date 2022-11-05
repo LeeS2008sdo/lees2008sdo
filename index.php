3e11cc01fda943bf04dbd35d32e394a2a50df33d
LeeS2008sdo <117508604+LeeS2008sdo@users.noreply.github.com>
2022-11-05 13:20:30 +0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php 
+ $p = '어서 오십시오!';
+
+ $name = 'Александр';
+ $surname = 'Ли';
+ $city = 'Ташкент';
+ $age = 33;
+
+include 'main.php';
+?>
---
+++ logo.inc.php
@@ -0,0 +1,3 @@
+<div class="logo"> 
+  <img src="img/2.png" width="100">
+</div>
